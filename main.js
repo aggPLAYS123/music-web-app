477bfc5332476962f5db3e8c0891b17b4376b9b0
aggPLAYS123 <93471052+aggPLAYS123@users.noreply.github.com>
2023-12-26 10:40:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    
+    video=createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+song1="";
+song2="";
+
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+function preload(){
+    song1=loadSound("believer.mp3");
+    song2=loadSound("khalasi.mp3");
+}
+
+function modelLoaded(){
+    console.log('poseNet Is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.length > 0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("LeftWrist X = "+leftWristX + "LeftWrist Y ="+leftWristY);
+        
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWrist X = "+rightWristX + "rightWrist Y ="+rightWristY);
+    }
+}
